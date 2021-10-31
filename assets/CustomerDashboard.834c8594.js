23af9970470873cf0e4d7b711c556c5a32f0d819
rasel-code-dev <raselmr005@gmail.com>
2021-10-31 12:29:17 +0600

    initial preview v14

---
+++ assets/CustomerDashboard.834c8594.js
@@ -0,0 +1,1 @@
+import{R as e}from"./vendor.32ae6308.js";import{B as a}from"./index.dc753515.js";import"./Dashboard.363176d2.js";import"./index.701e0ca0.js";const l=t=>{const{username:r}=t;return e.createElement("div",null,e.createElement("h1",null,"Dashboard HomePage"),e.createElement(a,{to:`/customer/${r}/create-seller-account`,type:"link"},"create-seller-account"))};export{l as default};
